0a896f070e823cca5d7649239e7be2a20e8f1f7c
Mashima <52222646+zimashima@users.noreply.github.com>
2019-11-13 15:35:41 -0500

    finished step one

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [member, setMember] = useState([])
+
+  const addNewMember = newMember => {
+    const newOne = {
+      name: newMember.name,
+      email: newMember.email,
+      role: newMember.role
+    };
+    setMember([...member, newOne]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="teamMemberList">
+      <h1>Meet my Team!</h1>
+
     </div>
   );
 }
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Form(){
+    return (
+        <form>
+            <input />
+            <input />
+            <select>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+            </select>
+        </form>
+    )
+}
